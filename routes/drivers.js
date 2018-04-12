d2b3435722131b064c38323a0a54d82e889e7b95
David Deller <david@tallygo.com>
2018-04-12 11:48:59 -0700

    Driver ETA endpoint

--- routes/drivers.js
+++ routes/drivers.js
@@ -3,7 +3,22 @@ var router = express.Router();
 var moment = require('moment');
 
 router.put('/current_location', function(req, res, next) {
+  if (req.body.latitude == null || req.body.longitude == null) {
+    res.sendStatus(400);
+    return;
+  }
+  
   console.log(moment().format() + " - Received driver current location: " + req.body.latitude + "," + req.body.longitude);
+  res.sendStatus(200);
+});
+
+router.put('/eta', function(req, res, next) {
+  if (req.body.ETA == null) {
+    res.sendStatus(400);
+    return;
+  }
+  
+  console.log(moment().format() + " - Received driver ETA: " + req.body.ETA);
   res.sendStatus(200);
 });
 
