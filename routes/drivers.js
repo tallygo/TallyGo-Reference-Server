90403395fcda5102f3d88915c96664117fb07d5c
David Deller <david@tallygo.com>
2018-04-17 14:53:30 -0700

    Driver route segment endpoint

--- routes/drivers.js
+++ routes/drivers.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
+var util = require('util');
+var _ = require('underscore');
 
 router.put('/current_location', function(req, res, next) {
   if (req.body.latitude == null || req.body.longitude == null) {
@@ -19,6 +21,20 @@ router.put('/eta', function(req, res, next) {
   }
   
   console.log(moment().format() + " - Received driver ETA: " + req.body.ETA);
+  res.sendStatus(200);
+});
+
+router.put('/route_segment', function(req, res, next) {
+  var routeSegment = req.body;
+  
+  if (routeSegment.points == null) {
+    res.sendStatus(400);
+    return;
+  }
+  
+  var printedInfo = util.inspect(_.extend(_.omit(routeSegment, 'points'), {'points': '(omitted for brevity)'}));
+  
+  console.log(moment().format() + " - Received driver route segment: " + printedInfo);
   res.sendStatus(200);
 });
 
